809c519e071d79042050a5168ebdb84e108e163c
Dry-Zer <56358156+Dry-Zer@users.noreply.github.com>
2019-11-20 19:00:42 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,30 +1,56 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client()
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const prefix = "*"
 
-bot.on("ready", () => {
-    console.log('Bot ON')
-    bot.user.setActivity(" | by $ad ", { type: "STREAMING", url: "https://www.twitch.tv/hya" });
+client.on('message', message => {
+
+  if (message.content.startsWith(prefix + 'mpall')){
+    let messageArray = message.content.split(" "); let cmd = messageArray[0]; let args = messageArray.slice(1);
+
+                if(message.channel.type === "dm") return;
+                if(message.deletable) message.delete();
+
+                let MpMessage = args.slice(0).join(" ");
+
+                message.guild.members.forEach(member => {
+                    member.send(MpMessage)
+    })
+ }
 });
 
-bot.on('message', message => {
-    if (message.author.bot) return;
-    if (message.channel.type === "dm") return;
+client.once('ready', () => {
+    console.log(`${client.user.tag} est connecté`)
+    client.user.setActivity('𝘓𝘈 𝘝𝘐𝘌 𝘘𝘜𝘖𝘕 𝘔𝘌𝘕𝘌', {type: "PLAYING"})
+})
 
-    let prefix = "-";
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
+client.on('message', message => {
+	if (message.content === '*ping') {
+		message.channel.send('Pong.');
+	}
+});
 
-    if (cmd === prefix + "mpall") {
-        
-        let argsulte = message.content.split(" ").slice(1);
-        let msge = argsulte.join(' ');
-
-        if (!msge) return message.channel.send("Précise un message bb :heart: ")
-        message.guild.members.map(m => m.send(msge))
-        message.delete()
+client.on('message', msg => {
+    if (msg.content === '*insulte') {
+      msg.reply('NIKE ZEBI');
     }
+  });
 
-});
+client.on('guildMemberAdd', async member => {
+    const channel = member.guild.channels.find('name', '?????????????????');
+    if (!channel) return;
+    channel.send(`Bienvenue sur le serveur ${member}, tu peux inviter des gens, pour recevoir ta récompense !`);
+    }
+    );
 
-bot.login("NjQ1NzAxNDg4MDg5NDk3NjMy.XdGafA.q43V_RM1M5H3E4IfzDFuBIfss-A")
+    client.on('message', function(message) {
+        if (message.content == "*clear") {
+            if (message.member.hasPermission("MANAGE_MESSAGES")) {
+                message.channel.fetchMessages()
+                   .then(function(list){
+                        message.channel.bulkDelete(list);
+                    }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})                        
+            }
+        }
+    });
+
+client.login('NjQ1NzAxNDg4MDg5NDk3NjMy.XdGafA.q43V_RM1M5H3E4IfzDFuBIfss-A')
